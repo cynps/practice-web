34fff927764b8e1dccc4e6a357a2b430250721ad
cynps <cnospy@gmail.com>
2023-01-29 23:36:03 +0900

    upd random-cat

--- ts-tutorial-next-app/random-cat/src/pages/index.tsx
+++ ts-tutorial-next-app/random-cat/src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
+const catImages: string[] = [
+  "https://cdn2.thecatapi.com/images/bpc.jpg",
+  "https://cdn2.thecatapi.com/images/eac.jpg",
+  "https://cdn2.thecatapi.com/images/6qi.jpg",
+];
+
+const randomCatImage = (): string => {
+  const index = Math.floor(Math.random() * catImages.length);
+  return catImages[index];
+}
+
 const IndexPage = () => {
-  return <img src="https://cdn2.thecatapi.com/images/bpc.jpg" />
+  const [catImageUrl, setCatImageUrl] = useState(
+    "https://cdn2.thecatapi.com/images/bpc.jpg"
+  );
+
+  const handleClick = () => {
+    setCatImageUrl(randomCatImage());
+  };
+  
+  return (
+    <div>
+      <button onClick={handleClick}>Today's cat</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
